dd0cd7bec151a823ccf99ab67ee364bd150824eb
kxngamun <dominickasanga@gmail.com>
2021-03-07 11:34:26 +0200

    updates

---
+++ app/src/main/java/com/example/chatapp/StartActivity.java
@@ -0,0 +1,57 @@
+package com.example.chatapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button btn_login;
+    Button btn_register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check is user is not null
+        if(firebaseUser != null) {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        btn_login = findViewById(R.id.login);
+        btn_register = findViewById(R.id.register);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
